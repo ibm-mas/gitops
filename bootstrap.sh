d65367367481e1a78923e6b1ee918a16e7c57edf
Caio Pereira <caiofcp@br.ibm.com>
2023-04-03 15:19:12 -0300

    Adding bootstrap script to deploy and configure gitops operator

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,23 +25,10 @@ done
 
 # 1. Install Openshift GitOps Operator
 
-# 1.1 Install Operator Group
-cat <<EOF | oc apply  -f -
-apiVersion: apps/v1
-kind: OperatorGroup
-metadata:
-  name: openshift-gitops
-  namespace: openshift-operators
-spec:  
-    channel: latest
-    installPlanApproval: Automatic
-    name: openshift-gitops-operator
-    source: redhat-operators
-    sourceNamespace: openshift-marketplace
-EOF
 # 1.1 Install Subscription
+echo 'Deploy Openshift GitOps Operator'
 cat <<EOF | oc apply  -f -
-apiVersion: apps/v1
+apiVersion: operators.coreos.com/v1alpha1
 kind: Subscription
 metadata:
   name: openshift-gitops
@@ -53,43 +40,120 @@ spec:
     source: redhat-operators
     sourceNamespace: openshift-marketplace
 EOF
+sleep 60
+oc wait --for=condition=Established crd/argocds.argoproj.io
 
-# 2. Create Secret Manager secret
 
+# # 2. Create Secret Manager secret
+echo 'Create Secret Manager Backend Secret'
 cat <<EOF | oc apply  -f -
-apiVersion: apps/v1
+apiVersion: v1
 kind: Secret
 metadata:
   name: openshift-gitops-vault
-  namespace: openshift-operators
-stringData: |    
-    AVP_IBM_INSTANCE_URL: $AVP_IBM_INSTANCE_URL
-    AVP_TYPE: ibmsecretsmanager
-    AVP_IBM_API_KEY: $IBMCLOUD_APIKEY
+  namespace: openshift-gitops
+stringData: 
+    vault.yml: |
+        AVP_IBM_INSTANCE_URL: $AVP_IBM_INSTANCE_URL
+        AVP_TYPE: ibmsecretsmanager
+        AVP_IBM_API_KEY: $IBMCLOUD_APIKEY
 EOF
 
-
-oc get namespace openshift-gitops --wait
-
 # 3. Create repo server SA
+echo 'Create ArgoCD repo server service account'
 cat <<EOF | oc apply  -f -
-apiVersion: apps/v1
+apiVersion: v1
 kind: ServiceAccount
 metadata:
   name: openshift-gitops-repo-sa
-  namespace: openshift-gitops
+  namespace: openshift-gitops  
 EOF
-# 4. Patch openshift-gitops with argocd-vault-plugin
 cat <<EOF | oc apply  -f -
-apiVersion: apps/v1
-kind: ArgoC
+kind: RoleBinding
+apiVersion: rbac.authorization.k8s.io/v1
 metadata:
   name: openshift-gitops
-  namespace: Subscription
-spec:  
-    channel: latest
-    installPlanApproval: Automatic
-    name: openshift-gitops-operator
-    source: redhat-operators
-    sourceNamespace: openshift-marketplace
+  namespace: openshift-gitops
+subjects:
+  - kind: ServiceAccount
+    name: openshift-gitops-repo-sa
+    namespace: openshift-gitops
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
 EOF
+# # 4. Patch openshift-gitops with argocd-vault-plugin
+echo 'Patch cluster ArgoCD'
+cat <<EOF | oc apply  -f -
+apiVersion: argoproj.io/v1alpha1
+kind: ArgoCD
+metadata:
+  name: openshift-gitops
+  namespace: openshift-gitops
+spec:   
+  repo:
+    initContainers:
+      - args:
+          - >-
+            wget -O argocd-vault-plugin
+            https://github.com/argoproj-labs/argocd-vault-plugin/releases/download/v1.13.1/argocd-vault-plugin_1.13.1_linux_amd64
+            && chmod +x argocd-vault-plugin && mv argocd-vault-plugin
+            /custom-tools/
+        command:
+          - sh
+          - '-c'
+        env:
+          - name: AVP_VERSION
+            value: 1.13.1
+        image: 'alpine:3.8'
+        name: download-tools
+        resources: {}
+        volumeMounts:
+          - mountPath: /custom-tools
+            name: custom-tools
+    mountsatoken: true
+    resources:
+      limits:
+        cpu: '1'
+        memory: 1Gi
+      requests:
+        cpu: 250m
+        memory: 256Mi
+    serviceaccount: openshift-gitops-repo-sa
+    volumeMounts:
+      - mountPath: /usr/local/bin/argocd-vault-plugin
+        name: custom-tools
+        subPath: argocd-vault-plugin
+      - mountPath: /home/argocd
+        name: vault
+    volumes:
+      - emptyDir: {}
+        name: custom-tools
+      - name: vault
+        secret:
+          secretName: openshift-gitops-vault
+  resourceExclusions: |
+    - apiGroups:
+      - tekton.dev
+      clusters:
+      - '*'
+      kinds:
+      - TaskRun
+      - PipelineRun
+    - apiGroups:
+      - cert-manager.io
+      clusters:
+      - '*'
+      kinds:
+      - Certificate
+      - ClusterIssuer
+      - Issuer  
+  configManagementPlugins: |-
+    - name: argocd-vault-plugin
+      generate:
+        command: ["argocd-vault-plugin"]
+        args: ["generate", "./", "-c", "/home/argocd/vault.yml"]  
+EOF
+
+oc wait --for=jsonpath='{.status.phase}'=Available argocd/openshift-gitops -n openshift-gitops

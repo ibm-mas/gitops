6648659694dac6281446fb38b8abe98d1a8179ee
Andrew Whitfield <WHITFIEA@uk.ibm.com>
2024-04-24 15:47:35 +0100

    Add actions

---
+++ build/bin/copy-gitops.sh
@@ -0,0 +1,41 @@
+#/bin/bash
+
+echo "build/bin/copy-gitops.sh -s <source ibm-mas/gitops directory> -t <target ibm-mas/gitops directory> "
+echo ""
+echo "Example usage: "
+echo "  build/bin/copy-gitops.sh -s /Users/whitfiea/Work/Git/ibm-mas/gitops -t /Users/whitfiea/Work/Git/ibm-mas/mas-gitops"
+echo ""
+
+# Process command line arguments
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    shift
+    case $key in
+        -s|--source)
+        SOURCE=$1
+        shift
+        ;;
+
+        -t|--target)
+        TARGET=$1
+        shift
+        ;;
+
+        *)
+        # unknown option
+        echo -e "\nUsage Error: Unsupported flag \"${key}\"\n\n"
+        exit 1
+        ;;
+    esac
+done
+
+: ${SOURCE?"Need to set -s|--source argument for source directory"}
+: ${TARGET?"Need to set -t|--target argument for target directory"}
+
+echo "Copying gitops"
+cp -vr ${SOURCE}/applications/* ${TARGET}/applications/
+cp -vr ${SOURCE}/root-applications/* ${TARGET}/root-applications/
+cp -vr ${SOURCE}/docs/* ${TARGET}/docs/
+cp -vr ${SOURCE}/README.md* ${TARGET}/README.md
+cp -vr ${SOURCE}/LICENSE ${TARGET}/LICENSE

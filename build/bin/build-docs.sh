aed9c5a795cd7fc646caffd669fe15f26c9a4138
Tom Klapiscak <asdasd>
2024-06-12 12:36:08 +0100

    fix

--- build/bin/build-docs.sh
+++ build/bin/build-docs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python -m pip install -q mkdocs mkdocs-redirects
-python -m pip install -q mkdocs mkdocs-macros-plugins
+python -m pip install -q mkdocs mkdocs-macros-plugin
 mkdocs build --verbose --clean
 
 # TODO: add back --strict

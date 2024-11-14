3b297a1011f3de7582c4f4e63c4e2edb257d5730
Nivedithaa Mahendran <nivedithaa.mahendran@ibm.com>
2024-11-14 17:30:08 +0530

    fix

--- instance-applications/120-ibm-db2u-database/files/Reg-Large_TBSP.sh
+++ instance-applications/120-ibm-db2u-database/files/Reg-Large_TBSP.sh
@@ -9,9 +9,9 @@ db2 alter tablespace MAXDATA convert to large
 db2 alter tablespace MAXINDEX convert to large
 
 
-db2 "select 'REORG TABLE '|| RTRIM(TABSCHEMA) || '.\"' || RTRIM(tabname)||'\" ;' from syscat.tables where tabschema = 'MAXIMO' and type ='T'" | grep REORG > ALL_TB_REORG.sql
-db2 "select 'REORG INDEXES ALL FOR TABLE '|| RTRIM(TABSCHEMA) || '.\"' || RTRIM(tabname)||'\" ;' from syscat.tables where tabschema = 'MAXIMO' and type ='T'" | grep REORG > ALL_IX_REORG.sql
-db2 "select 'RUNSTATS ON TABLE '|| RTRIM(TABSCHEMA) || '.\"' || RTRIM(tabname)||'\" ON ALL COLUMNS WITH DISTRIBUTION ON ALL COLUMNS AND DETAILED INDEXES ALL;' from syscat.tables where tabschema = 'MAXIMO' and type ='T'" | grep RUNSTATS > ALL_TB_RUNSTATS.sql
+db2 "select 'REORG TABLE '|| RTRIM(TABSCHEMA) || '.\"' || RTRIM(tabname)||'\" ;' from syscat.tables where tabschema not like  'SYS%' and type ='T'" | grep REORG > ALL_TB_REORG.sql
+db2 "select 'REORG INDEXES ALL FOR TABLE '|| RTRIM(TABSCHEMA) || '.\"' || RTRIM(tabname)||'\" ;' from syscat.tables where tabschema  not like  'SYS%' and type ='T'" | grep REORG > ALL_IX_REORG.sql
+db2 "select 'RUNSTATS ON TABLE '|| RTRIM(TABSCHEMA) || '.\"' || RTRIM(tabname)||'\" ON ALL COLUMNS WITH DISTRIBUTION ON ALL COLUMNS AND DETAILED INDEXES ALL;' from syscat.tables where tabschema not like  'SYS%' and type ='T'" | grep RUNSTATS > ALL_TB_RUNSTATS.sql
 
 db2 -tvf ALL_TB_REORG.sql | tee  | tee ALL_TB_REORG.OUT
 db2 -tvf ALL_IX_REORG.sql  | tee ALL_IX_REORG.OUT

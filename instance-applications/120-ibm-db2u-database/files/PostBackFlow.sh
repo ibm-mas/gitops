e2a33d684b63e70719a16ea069ed7a59d6b74f9d
Nikita Kumble <nikitakumble@gmail.com>
2026-08-20 15:22:52 +0530

    add if condition for manage specific sql queries

--- instance-applications/120-ibm-db2u-database/files/PostBackFlow.sh
+++ instance-applications/120-ibm-db2u-database/files/PostBackFlow.sh
@@ -10,7 +10,9 @@ SCRIPT=/mnt/backup/bin/PostBF_Scripts.sh
 
     echo "db2 connect to bludb" > ${SCRIPT}
     echo "db2 GRANT DBADM,CREATETAB,BINDADD,CONNECT,CREATE_NOT_FENCED_ROUTINE,IMPLICIT_SCHEMA,LOAD,CREATE_EXTERNAL_ROUTINE,QUIESCE_CONNECT ON DATABASE TO USER ${DEST_USER}" >> ${SCRIPT}
+if [[ ${DEST_USER} == *MANA* ]]; then
     echo "db2 GRANT USE OF TABLESPACE MAXDATA TO USER ${DEST_USER}" >> ${SCRIPT}
+fi
     echo "db2 GRANT DBADM WITHOUT DATAACCESS WITHOUT ACCESSCTRL ON DATABASE  TO USER ${DEST_USER}" >> ${SCRIPT}
     echo "db2 GRANT SECADM ON DATABASE  TO USER ${DEST_USER}" >> ${SCRIPT}
     echo "db2 GRANT DATAACCESS ON DATABASE  TO USER ${DEST_USER} " >> ${SCRIPT}
@@ -18,9 +20,11 @@ SCRIPT=/mnt/backup/bin/PostBF_Scripts.sh
     echo "db2 GRANT ACCESSCTRL ON DATABASE  TO USER ${DEST_USER} " >> ${SCRIPT}
 
 ### Update KAFKA
+if [[ ${DEST_USER} == *MANA* ]]; then
   echo "db2 \"update maximo.MSGHUBPROVIDERCFG set PROPVALUE=null where propname='BOOTSTRAPSERVERS' and provider='KAFKA'\" " >> ${SCRIPT}
   echo "db2 \"update maximo.MSGHUBPROVIDERCFG set PROPVALUE=null where propname='PASSWORD' and provider='KAFKA'\" "  >> ${SCRIPT}
   echo "db2 \"commit\" " >> ${SCRIPT}
+fi
   echo "  "
   echo "  " 
 
